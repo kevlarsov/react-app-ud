86da4dcfc09a66c22b87cd8da70221d6265d2a88
Kevin <103957894+kevlarsov@users.noreply.github.com>
2022-11-14 15:10:13 -0700

    x2

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -6,16 +6,18 @@ function ExpensesList(props) {
     return <h2 className="expenses-list__fallback">No expenses found!</h2>;
   }
 
-  return (<ul className="expenses-list">
-    {props.items.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ))}
-  </ul>);
+  return (
+    <ul className="expenses-list">
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </ul>
+  );
 }
 
 export default ExpensesList;
